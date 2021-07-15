b820a18e4d0dda64752bd86b6bcb3794848acd2e
kshitijahande <13883642+kshitijahande@users.noreply.github.com>
2021-07-15 13:53:56 +0530

    working copy of project on local
    print statements corrected
    clean code

---
+++ app.py
@@ -0,0 +1,35 @@
+import flask
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+# Use pickle to load in the pre-trained model
+with open(f'models_archive/tfidf.pickle', 'rb') as f:
+    vectorizer = pickle.load(f)
+
+with open(f'models_archive/sentiment_classifier.pkl', 'rb') as f:
+    model = pickle.load(f)    
+
+# Initialise the Flask app
+app = flask.Flask(__name__, template_folder='templates')
+
+# Set up the main route
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        # Just render the initial form, to get input
+        return(flask.render_template('main.html'))
+    
+    if flask.request.method == 'POST':
+        # Extract the input
+        user_set = flask.request.form['user_input']
+        print(user_set)
+        test_set = [user_set]
+        print(test_set)
+        new_test = vectorizer.transform(test_set)
+        prediction = model.predict(new_test)
+        print(prediction)
+        return flask.render_template('main.html',
+        							   result=prediction,
+                                     )
+
+if __name__ == '__main__':
+    app.run()
